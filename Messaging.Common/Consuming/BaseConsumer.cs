8c7dbb2c76a9c1596210e5e1173a8df9e5a7ea2d
HiSapril <2324802010207@student.tdmu.edu.vn>
2026-03-13 20:33:08 +0700

    Add Messaging.Common and fix project references

---
+++ Messaging.Common/Consuming/BaseConsumer.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+using Microsoft.Extensions.Hosting; // PHẢI CÓ THƯ VIỆN NÀY
+
+namespace Messaging.Common.Consuming
+{
+    // 1. Kế thừa BackgroundService để .NET có thể tự khởi chạy
+    public abstract class BaseConsumer<T> : BackgroundService
+    {
+        protected readonly IModel _channel; // Đổi sang protected để lớp con thấy được
+        protected readonly string _queue;
+
+        protected BaseConsumer(IModel channel, string queue)
+        {
+            _channel = channel;
+            _queue = queue;
+        }
+
+        // 2. BackgroundService yêu cầu thực thi hàm ExecuteAsync
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new AsyncEventingBasicConsumer(_channel);
+
+            consumer.Received += async (model, ea) =>
+            {
+                try
+                {
+                    var body = Encoding.UTF8.GetString(ea.Body.ToArray());
+                    var message = JsonSerializer.Deserialize<T>(body);
+
+                    // Gọi logic xử lý ở lớp con
+                    await HandleMessage(message!, ea.BasicProperties?.CorrelationId ?? string.Empty);
+
+                    _channel.BasicAck(ea.DeliveryTag, multiple: false);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"[Error] Failed to process message: {ex.Message}");
+                    _channel.BasicNack(ea.DeliveryTag, multiple: false, requeue: false);
+                }
+            };
+
+            _channel.BasicConsume(queue: _queue, autoAck: false, consumer: consumer);
+
+            return Task.CompletedTask;
+        }
+
+        protected abstract Task HandleMessage(T message, string correlationId);
+    }
+}
---
+++ Messaging.Common/Publishing/RabbitMqMessagePublisher.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Messaging.Common.Publishing
+{
+    public class RabbitMqMessagePublisher : IMessagePublisher
+    {
+        private readonly IModel _channel;
+
+        public RabbitMqMessagePublisher(IModel channel)
+        {
+            _channel = channel;
+        }
+
+        public void Publish<T>(T message, string exchangeName, string routingKey) where T : class
+        {
+            // Chuyển Object thành chuỗi JSON và mảng byte
+            var json = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(json);
+
+            // Gửi tin nhắn lên Exchange cụ thể
+            _channel.BasicPublish(exchange: exchangeName,
+                                 routingKey: routingKey,
+                                 basicProperties: null,
+                                 body: body);
+        }
+    }
+}
